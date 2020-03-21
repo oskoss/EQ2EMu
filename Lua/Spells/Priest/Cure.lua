d479a28cd230b8bb0785f373eb0c8297ea0f3c55
Image <>
2020-03-21 17:06:43 -0400

    naetz09 Templar Cure Spell fix
    CureDetrimentByType -> CureByType

--- Lua/Spells/Priest/Cure.lua
+++ Lua/Spells/Priest/Cure.lua
@@ -8,10 +8,10 @@
 
 function cast(Caster, Target)
 -- Dispels X levels of any hostile effects on target
-    CureDetrimentByType(1, 1, "", (GetLevel(Caster) * 1.08) + 1)
-    CureDetrimentByType(1, 2, "", (GetLevel(Caster) * 1.08) + 1)
-    CureDetrimentByType(1, 3, "", (GetLevel(Caster) * 1.08) + 1)
-    CureDetrimentByType(1, 4, "", (GetLevel(Caster) * 1.08) + 1)
+    CureByType(1, 1, "", (GetLevel(Caster) * 1.08) + 1)
+    CureByType(1, 2, "", (GetLevel(Caster) * 1.08) + 1)
+    CureByType(1, 3, "", (GetLevel(Caster) * 1.08) + 1)
+    CureByType(1, 4, "", (GetLevel(Caster) * 1.08) + 1)
 end
 
 function remove(Caster, Target)
