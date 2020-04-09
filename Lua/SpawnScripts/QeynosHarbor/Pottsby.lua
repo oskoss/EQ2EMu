17188358fc8bbe63eafe2ef4499ab0609d252993
Gogs <gogs@fake.local>
2020-04-09 00:07:26 -0700

    Qeynos Harbor Pathing

---
+++ Lua/SpawnScripts/QeynosHarbor/Pottsby.lua
@@ -0,0 +1,50 @@
+--[[
+	Script Name		:	pepper.lua
+	Script Purpose	:	Waypoint Path for pepper.lua
+	Script Author	:	Auto Generated
+	Script Date		:	04/05/2020 05:54:21 PM
+	Script Notes	:	Locations collected from Live
+--]]
+
+function spawn(NPC)
+	waypoints(NPC)
+end
+
+function hailed(NPC, Spawn)
+	FaceTarget(NPC, Spawn)
+end
+
+function respawn(NPC)
+end
+
+function waypoints(NPC)
+	MovementLoopAddLocation(NPC, 687.81, -20.4, -70.68, 2, 0)
+	MovementLoopAddLocation(NPC, 705.35, -21.07, -89.94, 2, 0)
+	MovementLoopAddLocation(NPC, 666.15, -20.27, -131.95, 2, 0)
+	MovementLoopAddLocation(NPC, 712.81, -20.98, -75.04, 2, 0)
+	MovementLoopAddLocation(NPC, 712.26, -21.07, -35.34, 2, 0)
+	MovementLoopAddLocation(NPC, 693.63, -20.64, -18.84, 2, 0)
+	MovementLoopAddLocation(NPC, 735.79, -21.03, -34.52, 2, 0)
+	MovementLoopAddLocation(NPC, 734.5, -21.38, -53.19, 2, 0)
+	MovementLoopAddLocation(NPC, 753.14, -21.56, -54.35, 2, 0)
+	MovementLoopAddLocation(NPC, 765.69, -20.64, 3.88, 2, 0)
+	MovementLoopAddLocation(NPC, 780.03, -21.39, -59.85, 2, 0)
+	MovementLoopAddLocation(NPC, 859.74, -25.36, -64.36, 2, 0)
+	MovementLoopAddLocation(NPC, 889.27, -25.36, -57.52, 2, 0)
+	MovementLoopAddLocation(NPC, 902.09, -25.37, -16.38, 2, 0)
+	MovementLoopAddLocation(NPC, 907.44, -25.52, 27.61, 2, 0)
+	MovementLoopAddLocation(NPC, 872.1, -25.5, 96.62, 2, 0)
+	MovementLoopAddLocation(NPC, 864.04, -25.61, 114.87, 2, 0)
+	MovementLoopAddLocation(NPC, 839.38, -25.5, 98.4, 2, 0)
+	MovementLoopAddLocation(NPC, 838.28, -25.45, 88.18, 2, 0)
+	MovementLoopAddLocation(NPC, 862.95, -25.37, 40.17, 2, 0)
+	MovementLoopAddLocation(NPC, 830.55, -23.31, 47.28, 2, 0)
+	MovementLoopAddLocation(NPC, 809.04, -23.35, 27.67, 2, 0)
+	MovementLoopAddLocation(NPC, 781.68, -23.3, 73.67, 2, 0)
+	MovementLoopAddLocation(NPC, 816.97, -23.31, 85.51, 2, 0)
+	MovementLoopAddLocation(NPC, 806.51, -23.35, 106.62, 2, 0)
+	MovementLoopAddLocation(NPC, 806.77, -22.8, 126.59, 2, 0)
+	MovementLoopAddLocation(NPC, 800.88, -20.98, 146.24, 2, 0)
+end
+
+
---
+++ Lua/SpawnScripts/QeynosHarbor/Warland.lua
@@ -0,0 +1,34 @@
+--[[
+	Script Name		:	Warland.lua
+	Script Purpose	:	Waypoint Path for Warland.lua
+	Script Author	:	Auto Generated
+	Script Date		:	04/08/2020 10:30:14 PM
+	Script Notes	:	Locations collected from Live
+--]]
+
+function spawn(NPC)
+	waypoints(NPC)
+end
+
+function hailed(NPC, Spawn)
+	FaceTarget(NPC, Spawn)
+end
+
+function respawn(NPC)
+end
+
+function waypoints(NPC)
+	MovementLoopAddLocation(NPC, 705.82, -20.64, -22.54, 2, 0)
+	MovementLoopAddLocation(NPC, 740.93, -21.44, -22.18, 2, 0)
+	MovementLoopAddLocation(NPC, 757.61, -20.88, -35.3, 2, 0)
+	MovementLoopAddLocation(NPC, 764.83, -21.84, -55.37, 2, 0)
+	MovementLoopAddLocation(NPC, 727.1, -21.24, -63.05, 2, 0)
+	MovementLoopAddLocation(NPC, 710.11, -21.06, -83.53, 2, 20)
+	MovementLoopAddLocation(NPC, 727.1, -21.24, -63.05, 2, 0)
+	MovementLoopAddLocation(NPC, 764.83, -21.84, -55.37, 2, 0)
+	MovementLoopAddLocation(NPC, 757.61, -20.88, -35.3, 2, 0)
+	MovementLoopAddLocation(NPC, 740.93, -21.44, -22.18, 2, 0)
+	MovementLoopAddLocation(NPC, 705.82, -20.64, -22.54, 2, 0)
+end
+
+
