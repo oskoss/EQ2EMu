d38970f2b96963c90d33bf5a84bfd9f9418dde96
root <cronikus@gmail.com>
2020-05-03 13:00:02 -0700

    add empty missing file to stop error message

---
+++ server/Quests/ForestRuins/UncoverTheCaches.lua
@@ -0,0 +1,21 @@
+
+function Init(Quest)
+
+end
+
+function Accepted(Quest, QuestGiver, Player)
+
+end
+
+function Declined(Quest, QuestGiver, Player)
+
+end
+
+function quest_complete(Quest, QuestGiver, Player)
+
+end
+
+function Reload(Quest, QuestGiver, Player, Step)
+
+end
+
