7894364a12cf37b4da4653f7c8b60a37b604ea8a
Image <>
2020-05-09 08:22:12 -0400

    neatz Mesmerize spell fix

--- server/Spells/Mage/Enchanter/Coercer/Mesmerize.lua
+++ server/Spells/Mage/Enchanter/Coercer/Mesmerize.lua
@@ -1,22 +1,20 @@
 --[[
     Script Name    : Spells/Mage/Enchanter/Coercer/Mesmerize.lua
-    Script Author  : John Adams
-    Script Date    : 2013.11.20 05:11:13
+    Script Author  : neatz09
+    Script Date    : 2020.05.08 10:05:53
     Script Purpose : 
                    : 
 --]]
 
--- Info from spell_display_effects (remove from script when done)
 -- Mesmerizes target
 -- Prevents AOE (except when direct) 
 -- Dispelled when target takes damage
 -- Epic targets gain an immunity to Mesmerize effects of 45.0 seconds and duration is reduced to 5.0 seconds.
 -- Resistibility increases against targets higher than level 29.
-
-function cast(Caster, Target, ControlType)
-    AddControlEffect(Target, ControlType) 
+function cast(Caster, Target)
+    AddControlEffect(Target, 1) 
 end
 
-function remove(Caster, Target, ControlType)
-    RemoveControlEffect(Target, ControlType) 
+function remove(Caster, Target)
+    RemoveControlEffect(Target, 1) 
 end
