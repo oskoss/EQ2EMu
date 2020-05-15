603b800e7fbdd8b18e832fb48323c1695f23c660
Image <>
2020-05-14 23:57:47 -0400

    Quest Suspicious Activity in Antonica by Tyllia Navar added
    Only outstanding item is the red book over the NPCs head doesn't appear until you get past the first return dialog (??).  There is a step required to return to the original quest npc.
    
    insert into quests set quest_id=3000, name='Suspicious Activity',type='Antonica',zone='Antonica',level=13,enc_level=0,description='Tyllia Navar is observing gnoll activity in southwestern Antonica and has asked for my help. She has asked that I slay some of the Sabertooth neophytes and runts that run along the coast south of her location. She has also asked that I search the boat they have been using to ferry supplies between the mainland and their islands.',spawn_id=121435,lua_script='Quests/Antonica/SuspiciousActivity.lua',completed_text='I did as Tyllia asked and slew the gnolls. Unfortunately there was nothing in the boat.';
    insert into quest_details set quest_id=3000,subtype='Coin',value=92;
    insert into quest_details set quest_id=3000,subtype='MaxCoin',value=99;
    insert into quest_details set quest_id=3000,subtype='Experience',value=350;
    insert into quest_details set quest_id=3000,subtype='Faction',value=85,faction_id=214;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=70606,quantity=1;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=70962,quantity=1;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=73108,quantity=1;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=73713,quantity=1;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=75317,quantity=1;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=80061,quantity=1;
    insert into quest_details set quest_id=3000,type='Reward',subtype='Selectable',value=89586,quantity=1;

---
+++ server/Quests/Antonica/SuspiciousActivity.lua
@@ -0,0 +1,76 @@
+--[[
+	Script Name		:	SuspiciousActivity.lua
+	Script Purpose	:	Handles the quest, "Suspicious Activity"
+	Script Author	:	
+	Script Date		:	
+	Script Notes	:	
+
+	Zone			:	Antonica
+	Quest Giver		:	Tyllia Navar
+	Preceded by		:	None
+	Followed by		:	None
+--]]
+
+local QuestID = 3000
+
+function Init(Quest)
+	SetQuestFeatherColor(Quest, 3)
+	AddQuestStepKill(Quest, 1, "I need to kill five Sabertooth runts.", 5, 100, "I need to do what Tyllia has asked of me.", 1221, 121248,120092)
+	AddQuestStepKill(Quest, 2, "I need to kill two Sabertooth neophytes.", 2, 100, "I need to do what Tyllia has asked of me.", 1220, 121269,120123)
+	AddQuestStep(Quest, 3, "I need to inspect the boat on the south shore south west of where Tyllia is.", 1, 100, "I need to do what Tyllia has asked of me.", 830)
+	AddQuestStepCompleteAction(Quest, 1, "Step1Complete")
+	AddQuestStepCompleteAction(Quest, 2, "Step2Complete")
+	AddQuestStepCompleteAction(Quest, 3, "Step3Complete")
+end
+
+function Step1Complete(Quest, QuestGiver, Player)
+	UpdateQuestStepDescription(Quest, 1, "I've killed five Sabertooth runts.")
+	CompleteStage1Check(Quest, Player)
+
+	--AddQuestStepKill(Quest, 2, "I need to kill ten snakes.", 10, 100, "I need to head out into Antonica and kill the vermin that infest the land; rats, snakes and beetles. Knight-Lieutenant Alesso said I should kill about ten of each.", 1908, 120068, 121226, 121347)
+	--AddQuestStepCompleteAction(Quest, 2, "Step2Complete")
+end
+
+function Step2Complete(Quest, QuestGiver, Player)
+	UpdateQuestStepDescription(Quest, 2, "I've killed two Sabertooth neophytes.")
+	CompleteStage1Check(Quest, Player)
+end
+
+function Step3Complete(Quest, QuestGiver, Player)
+	UpdateQuestStepDescription(Quest, 3, "I have inspected the boat and found it mostly empty.")
+	CompleteStage1Check(Quest, Player)
+end
+
+function CompleteStage1Check(Quest, Player)
+	if QuestStepIsComplete(Player, QuestID, 1) and QuestStepIsComplete(Player, QuestID, 2) and QuestStepIsComplete(Player, QuestID, 3) then
+		UpdateQuestTaskGroupDescription(Quest, 1, "I have done what Tyllia asked of me.")
+		AddQuestStep(Quest, 4, "I need to return to Tyllia now that I have done all that she asked.", 1, 100, "I have done what Tyllia asked of me.", 11)
+		AddQuestStepCompleteAction(Quest, 4, "Step4Complete")
+	end
+end
+
+function Step4Complete(Quest, QuestGiver, Player)
+	UpdateQuestStepDescription(Quest, 4, "I have spoken with Tyllia.")
+	UpdateQuestDescription(Quest, "I did as Tyllia asked and slew the gnolls. Unfortunately there was nothing in the boat.")
+	GiveQuestReward(Quest, Player)
+end
+
+function Reload(Quest, QuestGiver, Player, Step)
+	if Step == 1 then
+		Step1Complete(Quest, QuestGiver, Player)
+	elseif Step == 2 then
+		Step2Complete(Quest, QuestGiver, Player)
+	elseif Step == 3 then
+		Step3Complete(Quest, QuestGiver, Player)
+	elseif Step == 4 then
+		QuestComplete(Quest, QuestGiver, Player)
+	end
+end
+
+function Accepted(Quest, QuestGiver, Player)
+	-- Add dialog here for when the quest is accepted
+end
+
+function Declined(Quest, QuestGiver, Player)
+	-- Add dialog here for when the quest is declined
+end
