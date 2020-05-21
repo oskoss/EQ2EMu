91b886202ab9ce4bb6c98f23a22eac4a16dc64c3
Image <>
2020-05-20 21:55:32 -0400

    Movement Loop Generator added
    - fixed crash on subsequent load of file
    - fixed Settings.cs missing from project (not needed)

---
+++ EQ2/devtools/Movement Loop Generator/Movement Loop Generator 2.0/bin/Debug/ire_minion10.lua
@@ -0,0 +1,56 @@
+--[[
+	Script Name		:	ire_minion10.lua
+	Script Purpose	:	Waypoint Path for ire_minion10.lua
+	Script Author	:	Auto Generated
+	Script Date		:	05/20/2020 09:49:29 PM
+	Script Notes	:	Locations collected from Live
+--]]
+
+function spawn(NPC)
+	waypoints(NPC)
+end
+
+function hailed(NPC, Spawn)
+	FaceTarget(NPC, Spawn)
+end
+
+function respawn(NPC)
+end
+
+function waypoints(NPC)
+	MovementLoopAddLocation(NPC, 165.44, 6.89, -51.63, 2, 0)
+	MovementLoopAddLocation(NPC, 163.87, 6.89, -53.39, 2, 0)
+	MovementLoopAddLocation(NPC, 169.71, 7.09, -55.82, 2, 0)
+	MovementLoopAddLocation(NPC, 167.8, 7.01, -57.66, 2, 0)
+	MovementLoopAddLocation(NPC, 168.4, 7.24, -54.97, 2, 0)
+	MovementLoopAddLocation(NPC, 166.34, 7.24, -52.81, 2, 0)
+	MovementLoopAddLocation(NPC, 166.05, 6.82, -56.79, 2, 0)
+	MovementLoopAddLocation(NPC, 168, 7.89, -53.02, 2, 0)
+	MovementLoopAddLocation(NPC, 164.47, 6.83, -55.21, 2, 0)
+	MovementLoopAddLocation(NPC, 167.37, 6.8, -55.15, 2, 0)
+	MovementLoopAddLocation(NPC, 165.28, 6.79, -55.51, 2, 0)
+	MovementLoopAddLocation(NPC, 164.06, 6.89, -52.59, 2, 0)
+	MovementLoopAddLocation(NPC, 168.66, 6.97, -56.92, 2, 0)
+	MovementLoopAddLocation(NPC, 168.56, 7.24, -54.62, 2, 0)
+	MovementLoopAddLocation(NPC, 166.27, 7.24, -52.51, 2, 0)
+	MovementLoopAddLocation(NPC, 168.45, 7.24, -54.6, 2, 0)
+	MovementLoopAddLocation(NPC, 166.98, 6.8, -54.96, 2, 0)
+	MovementLoopAddLocation(NPC, 168.45, 7.24, -54.6, 2, 0)
+	MovementLoopAddLocation(NPC, 166.27, 7.24, -52.51, 2, 0)
+	MovementLoopAddLocation(NPC, 168.56, 7.24, -54.62, 2, 0)
+	MovementLoopAddLocation(NPC, 168.66, 6.97, -56.92, 2, 0)
+	MovementLoopAddLocation(NPC, 164.06, 6.89, -52.59, 2, 0)
+	MovementLoopAddLocation(NPC, 165.28, 6.79, -55.51, 2, 0)
+	MovementLoopAddLocation(NPC, 167.37, 6.8, -55.15, 2, 0)
+	MovementLoopAddLocation(NPC, 164.47, 6.83, -55.21, 2, 0)
+	MovementLoopAddLocation(NPC, 168, 7.89, -53.02, 2, 0)
+	MovementLoopAddLocation(NPC, 166.05, 6.82, -56.79, 2, 0)
+	MovementLoopAddLocation(NPC, 166.34, 7.24, -52.81, 2, 0)
+	MovementLoopAddLocation(NPC, 168.4, 7.24, -54.97, 2, 0)
+	MovementLoopAddLocation(NPC, 167.8, 7.01, -57.66, 2, 0)
+	MovementLoopAddLocation(NPC, 169.71, 7.09, -55.82, 2, 0)
+	MovementLoopAddLocation(NPC, 163.87, 6.89, -53.39, 2, 0)
+	MovementLoopAddLocation(NPC, 165.44, 6.89, -51.63, 2, 0)
+end
+
+
