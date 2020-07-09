2c8258ad3858a073a06454f327fb140b2983b3a5
devn00b <deveq2emu@gmail.com>
2020-07-09 02:54:50 +0000

    Start of Elddar Grove Pass 1

--- server/SpawnScripts/ElddarGrove/GuardKaladenth.lua
+++ server/SpawnScripts/ElddarGrove/GuardKaladenth.lua
@@ -6,6 +6,10 @@
 	Script Notes	: Auto-Generated Conversation from PacketParser Data
 --]]
 
+function spawn(NPC)
+	waypoints(NPC)
+end
+
 function hailed(NPC, Spawn)
 	FaceTarget(NPC, Spawn)
 	choice = math.random(1,2)
@@ -19,3 +23,19 @@ function hailed(NPC, Spawn)
 
 end
 
+function waypoints(NPC)
+	MovementLoopAddLocation(NPC, 594.35, -21.42, -257.29, 2, 5)
+	MovementLoopAddLocation(NPC, 580.75, -20.84, -259, 2, 0)
+	MovementLoopAddLocation(NPC, 553.51, -20.04, -277.93, 2, 0)
+	MovementLoopAddLocation(NPC, 518.75, -20.64, -286.51, 2, 0)
+	MovementLoopAddLocation(NPC, 550.5, -21.45, -311.58, 2, 0)
+	MovementLoopAddLocation(NPC, 545.01, -21.17, -325.85, 2, 0)
+	MovementLoopAddLocation(NPC, 530.36, -21.04, -314.28, 2, 0)
+	MovementLoopAddLocation(NPC, 520.82, -20.75, -290.82, 2, 0)
+	MovementLoopAddLocation(NPC, 538.52, -20.27, -282.51, 2, 0)
+	MovementLoopAddLocation(NPC, 550.93, -20.18, -281.09, 2, 0)
+	MovementLoopAddLocation(NPC, 555.04, -20.01, -277.3, 2, 0)
+	MovementLoopAddLocation(NPC, 574.43, -20.55, -266.52, 2, 0)
+	MovementLoopAddLocation(NPC, 584.22, -20.78, -258.42, 2, 0)
+	MovementLoopAddLocation(NPC, 594.35, -21.42, -257.29, 2, 5)
+end
---
+++ server/SpawnScripts/ElddarGrove/grove_deer.lua
@@ -0,0 +1,50 @@
+--[[
+	Script Name		:	gove_deer.lua
+	Script Purpose	:	Waypoint Path for gove_deer.lua
+	Script Author	:	Devn00b
+	Script Date		:	07/05/2020 05:56:47 PM
+	Script Notes	:	Locations collected from Live
+--]]
+
+function spawn(NPC)
+	waypoints(NPC)
+end
+
+function hailed(NPC, Spawn)
+	FaceTarget(NPC, Spawn)
+end
+
+function respawn(NPC)
+end
+
+function waypoints(NPC)
+	MovementLoopAddLocation(NPC, 543.4, -17.04, -390.35, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 537.12, -17.14, -401.36, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 531.58, -19.29, -422.82, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 538.17, -17.09, -396.89, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 533.23, -17.21, -395.97, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 529.71, -17.42, -388.18, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 545.71, -18.94, -365.65, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 522.76, -20.61, -287.89, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 545.43, -20.07, -278.05, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 588.75, -20.8, -253.47, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 540.07, -20.65, -292.93, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 555.29, -21.69, -313.03, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 558.67, -20.5, -341.94, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 572.15, -22.08, -357.42, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 558.67, -20.5, -341.94, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 555.29, -21.69, -313.03, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 540.07, -20.65, -292.93, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 588.75, -20.8, -253.47, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 545.43, -20.07, -278.05, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 522.76, -20.61, -287.89, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 545.71, -18.94, -365.65, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 529.71, -17.42, -388.18, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 533.23, -17.21, -395.97, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 538.17, -17.09, -396.89, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 531.58, -19.29, -422.82, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 537.12, -17.14, -401.36, 2, math.random(0,10))
+	MovementLoopAddLocation(NPC, 543.4, -17.04, -390.35, 2, math.random(0,10))
+end
+
+
