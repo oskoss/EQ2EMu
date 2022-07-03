780dd2f02df72367c9ab2a1315f54999b245943d
Emagi <image.emagi@gmail.com>
2022-07-02 21:28:41 -0400

    missing column  for character items

--- DB/updates/character_items_updatedcolumn_june30_2022.sql
+++ DB/updates/character_items_updatedcolumn_june30_2022.sql
@@ -1,1 +1,2 @@
 alter table character_items add column last_saved timestamp default current_timestamp on update current_timestamp;
+alter table character_items add column created timestamp default current_timestamp;
