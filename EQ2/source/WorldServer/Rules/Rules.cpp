d060a6055beed016bf6fa2842594093d5b0f7b31
Emagi <image.emagi@gmail.com>
2022-09-19 21:52:25 -0400

    increased min Y offset since we can't account for underwater or lava

--- EQ2/source/WorldServer/Rules/Rules.cpp
+++ EQ2/source/WorldServer/Rules/Rules.cpp
@@ -330,7 +330,7 @@ void RuleManager::Init()
 	RULE_INIT(R_Zone, WeatherTimer, "60000");						// default: 1 minute
 	RULE_INIT(R_Zone, SpawnDeleteTimer, "30000");					// default: 30 seconds, how long a spawn pointer is held onto after being removed from the world before deleting it
 	RULE_INIT(R_Zone, UseMapUnderworldCoords, "1"); // use maps lowest Y coordinate to establish underworld markers
-	RULE_INIT(R_Zone, MapUnderworldCoordOffset, "-50.0"); // adds (or in the case of negative value subtracts) so that the underworld marker is lower when map is using its lowest Y coordinate
+	RULE_INIT(R_Zone, MapUnderworldCoordOffset, "-200.0"); // adds (or in the case of negative value subtracts) so that the underworld marker is lower when map is using its lowest Y coordinate
 	
 	RULE_INIT(R_Loot, LootRadius, "5.0");
 	RULE_INIT(R_Loot, AutoDisarmChest, "1");
