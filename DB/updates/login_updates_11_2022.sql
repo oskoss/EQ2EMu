2b92b9865eb49539504b450e946d6f657565aa38
Devn00b <devn00b@eqpvp.com>
2022-11-26 13:15:24 -0800

    SQL update for LoginServer support for max chars, and vet exp bonus display

---
+++ DB/updates/login_updates_11_2022.sql
@@ -0,0 +1,2 @@
+INSERT INTO `login_config` (`config_name`, `config_value`) VALUES ('max_characters_per_account', '30');
+INSERT INTO `login_config` (`config_name`, `config_value`) VALUES ('max_level_for_vet_reward', '20');
