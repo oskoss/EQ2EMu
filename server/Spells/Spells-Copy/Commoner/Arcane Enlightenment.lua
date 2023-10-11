2df54e57e62817a813b2189342871804207056d3
Emagi <image.emagi@gmail.com>
2023-10-10 22:52:38 -0400

    remove spells copy

--- server/Spells/Spells-Copy/Commoner/Arcane Enlightenment.lua
+++
@@ -1,9 +0,0 @@
---[[
-    Script Name    : Spells/Commoner/Arcane Enlightenment.lua
-    Script Author  : neatz09
-    Script Date    : 2020.03.22 08:03:09
-    Script Purpose : 
-                   : 
---]]
-
--- Info from spell_display_effects (remove from script when done)
